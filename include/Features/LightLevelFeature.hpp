15bca87b886c422cccf7444abd66466c60171049
Gandalf-pro <asoc04@hotmail.com>
2025-01-01 19:50:45 +0300

    just use a max light level

--- include/Features/LightLevelFeature.hpp
+++ include/Features/LightLevelFeature.hpp
@@ -3,21 +3,22 @@
 
 #include <General.hpp>
 
+int maxLightLevel = 1200;
+
 namespace home {
 class LightLevelFeature : public Feature {
    private:
     /* data */
     int lightLevel = 0;
-    const long A = 200;  // Dark resistance in KΩ
-    const int B = 5;     // Light resistance (10 Lux) in KΩ
-    const int Rc = 10;   // Calibration resistance in KΩ
-    const int maxLightLevel = ((long)1023 * A * 10) / ((long)B * Rc * (1024 - 1023));
 
     // Pins
     uint8_t pin;
 
     int getLightLevel(int V) {
-        return map(((long)V * A * 10) / ((long)B * Rc * (1024 - V)), 0, maxLightLevel, 0, 100);
+        if (V > maxLightLevel) {
+            V = maxLightLevel;
+        }
+        return map(V, 0, maxLightLevel, 0, 100);
     }
 
    public:
